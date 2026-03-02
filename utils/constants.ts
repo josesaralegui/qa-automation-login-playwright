e1c0ca40cdaed7e026dae8714bab6595062e0105
José Saralegui <josesaralegui@hotmail.com>
2026-03-01 22:16:37 -0300

    updating constants message for logout

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,6 +2,6 @@ export const VALID_USERNAME = 'tomsmith';
 export const VALID_PASSWORD = 'SuperSecretPassword!';
 
 export const SUCCESS_LOGIN_MESSAGE = 'You logged into a secure area!';
-export const SUCCESS_LOGOUT_MESSAGE = 'You logged into a secure area!';
+export const SUCCESS_LOGOUT_MESSAGE = 'You logged out of the secure area!';
 export const INVALID_USERNAME_MESSAGE = 'Your username is invalid!';
 export const INVALID_PASSWORD_MESSAGE = 'Your password is invalid!';
